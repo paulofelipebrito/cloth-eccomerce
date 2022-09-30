9ac71cdacedd2a00f4f934fe66b01f4535bc7308
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-09-30 09:17:27 -0300

    feat: change shop-data + setting into the db once

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,13 +1,19 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
+import SHOP_DATA from '../shop-data';
 
-import PRODUCTS from '../shop-data.json';
+import { addCollectionAndDocuments } from '../utils/firebase/firebase.utils';
 
 export const ProductsContext = createContext({
-  products: [],
+  products: {},
 });
 
 export const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState(PRODUCTS);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    addCollectionAndDocuments('categories', SHOP_DATA)
+  }, []);
+
   const value = { products };
   return (
     <ProductsContext.Provider value={value}>
