5e5081b0511c6cf709d52f492736eb2e8123d675
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-10-03 08:18:58 -0300

    feat: root-saga

--- src/store/root-saga.js
+++ src/store/root-saga.js
@@ -3,6 +3,7 @@ import { all, call } from 'redux-saga/effects';
 import { categoriesSaga } from './categories/category.saga';
 import { userSagas } from './user/user.saga';
 
+//generator function: ability to "pause" functions 
 export function* rootSaga() {
   yield all([call(categoriesSaga), call(userSagas)]);
 }
