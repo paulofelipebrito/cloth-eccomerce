0fc799fe3c7d55e52477f7b370071d967c58f3ec
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-10-05 07:39:10 -0300

    feat: change js to ts in category reducer

--- src/store/categories/category.selector.ts
+++ src/store/categories/category.selector.ts
@@ -1,7 +1,12 @@
 import { createSelector } from 'reselect';
+
+import { RootState } from '../store';
+import { CategoriesState } from './category.reducer';
+import { CategoryMap } from './category.types';
 //memorization'
 //Avoid unnecessary re-renders
-const selectCategoryReducer = (state) => state.categories;
+const selectCategoryReducer = (state: RootState): CategoriesState =>
+  state.categories;
 
 export const selectCategories = createSelector(
   [selectCategoryReducer],
@@ -10,12 +15,12 @@ export const selectCategories = createSelector(
 
 export const selectCategoriesMap = createSelector(
   [selectCategories],
-  (categories) =>
+  (categories): CategoryMap =>
     categories.reduce((acc, category) => {
       const { title, items } = category;
       acc[title.toLowerCase()] = items;
       return acc;
-    }, {})
+    }, {} as CategoryMap)
 );
 
 export const selectCategoriesIsLoading = createSelector(
