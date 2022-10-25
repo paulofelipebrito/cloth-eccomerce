1a9e48453fd7c633107c8aff46a890acc9c77760
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-10-25 07:45:25 -0300

    feat: add global style

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import Shop from './pages/shop/shop.component';
 import Checkout from './pages/checkout/checkout.component';
 import { checkUserSession } from './store/user/user.action';
 
+import { GlobalStyle } from './global.styles'
+
 const App = () => {
   const dispatch = useDispatch();
 
@@ -17,14 +19,18 @@ const App = () => {
   }, [dispatch]);
 
   return (
-    <Routes>
-      <Route path='/' element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path='shop/*' element={<Shop />} />
-        <Route path='auth' element={<Authentication />} />
-        <Route path='checkout' element={<Checkout />} />
-      </Route>
-    </Routes>
+    <>
+      <GlobalStyle />
+        <Routes>
+        <Route path='/' element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path='shop/*' element={<Shop />} />
+          <Route path='auth' element={<Authentication />} />
+          <Route path='checkout' element={<Checkout />} />
+        </Route>
+      </Routes>
+    </>
+    
   );
 };
 
